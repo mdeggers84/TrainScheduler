ef2373102361f2fb550551c177bba536a1dccb27
Michael Eggers <mdeggers84@gmail.com>
2017-04-08 23:43:43 -0400

    :smiley_cat:

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
 	var database = firebase.database();
 	var interval = setInterval(updateTable, 60000);
 
+	// google authentication
 	var provider = new firebase.auth.GoogleAuthProvider();
 
 	firebase.auth().signInWithPopup(provider).then(function(result) {
@@ -30,30 +31,6 @@ $(document).ready(function() {
 		var credential = error.credential;
 		// ...
 	});
-
-	/*
-
-	var provider = new firebase.auth.GithubAuthProvider();
-	
-	// sets up gitHub authentication (might switch to google later)
-	firebase.auth().signInWithPopup(provider).then(function(result) {
-	  // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-	  var token = result.credential.accessToken;
-	  // The signed-in user info.
-	  var user = result.user;
-	  // ...
-	}).catch(function(error) {
-	  // Handle Errors here.
-	  var errorCode = error.code;
-	  var errorMessage = error.message;
-	  // The email of the user's account used.
-	  var email = error.email;
-	  // The firebase.auth.AuthCredential type that was used.
-	  var credential = error.credential;
-	  // ...
-	});
-
-	*/
 
 	// posts data to webPage
 	function postData(trainData, trainKey) {
@@ -122,7 +99,3 @@ $(document).ready(function() {
 		postData(childSnapshot.val(), childSnapshot.key);
 	});
 });
-
-
-
-
